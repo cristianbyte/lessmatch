04cc8dc68b4be4ad50e893f84b9c2afc05657e14
cristianbyte <charlsmaritz@gmail.com>
2024-04-10 18:20:01 -0500

    Implement JWT authentication and security configuration; update dependencies and remove unused CORS and WebSocket configurations.

--- src/main/java/com/uni_verso/uni_verso/api/controller/UserController.java
+++ src/main/java/com/uni_verso/uni_verso/api/controller/UserController.java
@@ -1,5 +1,8 @@
 package com.uni_verso.uni_verso.api.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -15,9 +18,9 @@ import org.springframework.web.bind.annotation.RestController;
 import com.uni_verso.uni_verso.api.dto.request.UserRequest;
 import com.uni_verso.uni_verso.api.dto.response.UserResponse;
 import com.uni_verso.uni_verso.infrastructure.abstract_service.IUserService;
+import com.uni_verso.uni_verso.util.JwtUtils;
 
 import lombok.AllArgsConstructor;
-
 
 @RestController
 @RequestMapping("/user")
@@ -25,31 +28,37 @@ import lombok.AllArgsConstructor;
 public class UserController {
 
     private final IUserService userService;
+    private final JwtUtils jwtUtils;
 
-    @PostMapping
-    public ResponseEntity<UserResponse> create(@Validated @RequestBody UserRequest request) {
+    @PostMapping("/auth")
+    public ResponseEntity<Map<String, Object>> create(@Validated @RequestBody UserRequest request) {
         UserResponse userResponse = this.userService.create(request);
-        return ResponseEntity.status(HttpStatus.CREATED).body(userResponse);
+        String token = jwtUtils.createToken(userResponse);
+
+        Map<String, Object> response = new HashMap<>();
+        response.put("user", userResponse);
+        response.put("token", token);
+        System.out.println("Generated token: " + token);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<UserResponse> get(@PathVariable String id){
+    public ResponseEntity<UserResponse> get(@PathVariable String id) {
         UserResponse userResponse = this.userService.getById(id);
         return ResponseEntity.ok(userResponse);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<UserResponse> update(@PathVariable String id, @Validated @RequestBody UserRequest request){
+    public ResponseEntity<UserResponse> update(@PathVariable String id, @Validated @RequestBody UserRequest request) {
         UserResponse userResponse = this.userService.update(id, request);
         return ResponseEntity.status(HttpStatus.OK).body(userResponse);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> delete(@PathVariable String id){
+    public ResponseEntity<Void> delete(@PathVariable String id) {
         this.userService.delete(id);
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 
-
-    
 }
