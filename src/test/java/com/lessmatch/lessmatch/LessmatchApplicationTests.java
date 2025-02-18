72f6fe39864c604cdfcd59cced47e4934139c7cd
cristianbyte <charlsmaritz@gmail.com>
2025-02-17 21:28:12 -0500

    first commit

---
+++ src/test/java/com/lessmatch/lessmatch/LessmatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lessmatch.lessmatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LessmatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
