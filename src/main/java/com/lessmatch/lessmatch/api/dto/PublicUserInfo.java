bd5295649659d5e6949f2ea443b8f8f7c4cb37ca
cristianbyte <charlsmaritz@gmail.com>
2025-02-20 11:07:48 -0500

    logic

--- src/main/java/com/lessmatch/lessmatch/api/dto/PublicUserInfo.java
+++ src/main/java/com/lessmatch/lessmatch/api/dto/PublicUserInfo.java
@@ -1,5 +1,14 @@
 package com.lessmatch.lessmatch.api.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class PublicUserInfo {
     private String name;
     private String icon;
