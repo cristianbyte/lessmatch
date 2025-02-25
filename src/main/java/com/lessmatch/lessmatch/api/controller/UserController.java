5d8df055cf9c0443477648e9ce9354fab037d41f
cristianbyte <charlsmaritz@gmail.com>
2025-02-24 23:33:38 -0500

    udpate controller user

--- src/main/java/com/lessmatch/lessmatch/api/controller/UserController.java
+++ src/main/java/com/lessmatch/lessmatch/api/controller/UserController.java
@@ -3,9 +3,11 @@ package com.lessmatch.lessmatch.api.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,10 +32,24 @@ public class UserController {
         return ResponseEntity.status(HttpStatus.CREATED).body(userResponse);
     }
 
-    @GetMapping("/login/{id}")
+    @GetMapping("/{id}")
     public ResponseEntity<UserResponse> get(@PathVariable String id){
         UserResponse userResponse = this.userService.getById(id);
         return ResponseEntity.status( HttpStatus.FOUND).body(userResponse);
     }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<UserResponse> update(@PathVariable String id, @Validated @RequestBody UserRequest request){
+        UserResponse userResponse = this.userService.update(id, request);
+        return ResponseEntity.status(HttpStatus.OK).body(userResponse);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable String id){
+        this.userService.delete(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+
+
     
 }
