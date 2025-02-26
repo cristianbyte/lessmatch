de0de6f823b66cc3e1b5fbf3f451898bd3b12e1b
cristianbyte <charlsmaritz@gmail.com>
2025-02-25 20:28:36 -0500

    songs feature

---
+++ src/main/java/com/lessmatch/lessmatch/api/controller/SongController.java
@@ -0,0 +1,40 @@
+package com.lessmatch.lessmatch.api.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.lessmatch.lessmatch.api.dto.SongBasicInfo;
+import com.lessmatch.lessmatch.api.dto.request.SongRequest;
+import com.lessmatch.lessmatch.infrastructure.abstract_service.ISongService;
+
+import lombok.AllArgsConstructor;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/song")
+public class SongController {
+
+    private final ISongService songService;
+
+    @GetMapping("/popular")
+    public ResponseEntity<?> getMostPopularSongs() {
+        return ResponseEntity.ok(this.songService.getMostPopularSongs());
+    }
+    
+    @PostMapping
+    public ResponseEntity<SongBasicInfo> create(@Validated @RequestBody SongRequest request) {
+        SongBasicInfo songResponse = this.songService.create(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(songResponse);
+    }
+
+
+
+
+
+}
--- src/main/java/com/lessmatch/lessmatch/infrastructure/abstract_service/ISongService.java
+++ src/main/java/com/lessmatch/lessmatch/infrastructure/abstract_service/ISongService.java
@@ -1,8 +1,10 @@
 package com.lessmatch.lessmatch.infrastructure.abstract_service;
 
+import java.util.List;
+
 import com.lessmatch.lessmatch.api.dto.SongBasicInfo;
 import com.lessmatch.lessmatch.api.dto.request.SongRequest;
 
-public interface ISongService extends CrudService<SongRequest, SongBasicInfo, Long> {
-    
+public interface ISongService extends CrudService<SongRequest, SongBasicInfo, Long>{
+    List<SongBasicInfo> getMostPopularSongs();
 }
