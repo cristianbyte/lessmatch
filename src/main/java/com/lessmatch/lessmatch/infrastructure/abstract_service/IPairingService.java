1c0fe38f1217cad7e90bba37a4626ebbad6fcf17
cristianbyte <charlsmaritz@gmail.com>
2025-03-03 17:09:47 -0500

    almost: fully functional features

--- src/main/java/com/lessmatch/lessmatch/infrastructure/abstract_service/IPairingService.java
+++ src/main/java/com/lessmatch/lessmatch/infrastructure/abstract_service/IPairingService.java
@@ -1,8 +1,11 @@
 package com.lessmatch.lessmatch.infrastructure.abstract_service;
 
+import java.util.List;
+
 import com.lessmatch.lessmatch.api.dto.request.LineSelectionRequest;
 import com.lessmatch.lessmatch.api.dto.request.PairingRequest;
 import com.lessmatch.lessmatch.api.dto.response.PairingResponse;
+import com.lessmatch.lessmatch.api.dto.response.PairingResponseFull;
 import com.lessmatch.lessmatch.infrastructure.abstract_service.generic.CreateService;
 import com.lessmatch.lessmatch.infrastructure.abstract_service.generic.DeleteService;
 import com.lessmatch.lessmatch.infrastructure.abstract_service.generic.ReadService;
@@ -13,4 +16,6 @@ public interface IPairingService extends
     DeleteService<Long>{
         PairingResponse udpate(String pairingCode, String pairedUserId);
         PairingResponse updateLineSelections(LineSelectionRequest request);
+        List<PairingResponse> getPairingsByUserId(String userId);
+        PairingResponseFull getFullPairingByCode(String pairingCode);
 }
