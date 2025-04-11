36b129908559acaa1d7ca98f05e168d7a7572df1
cristianbyte <charlsmaritz@gmail.com>
2025-04-11 11:07:58 -0500

    udpate

--- src/main/java/com/uni_verso/uni_verso/config/SecurityConfig.java
+++ src/main/java/com/uni_verso/uni_verso/config/SecurityConfig.java
@@ -42,10 +42,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList(
-                "https://uni-verso.vercel.app",
-                "https://universo.coder.red"
-        ));
+        configuration.setAllowedOrigins(Arrays.asList("*"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "Accept"));
         configuration.setAllowCredentials(true);
